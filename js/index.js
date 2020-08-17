0b3329042e33de33e9cbdc2272e4ea3fba10fa73
androuino <androuino.info@gmail.com>
2020-08-17 09:56:47 +0900

    change content

--- js/index.js
+++ js/index.js
@@ -1,28 +1,15 @@
 const template = document.getElementsByTagName('template')[0];
 const img = document.createElement('img');
-img.id = "leftMainImg";
-img.src = "assets/steak.jpg"
-img.alt = "Steak"
+img.id = "talkLogo";
+img.src = "assets/logo.png"
+img.alt = "Talk"
 const desc = document.createElement('div')
-desc.id = "leftMainImgDesc";
+desc.id = "talkLogo";
 const name = document.createElement('h1');
-name.id = "steakMenu"
-const story = document.createElement('h2');
-story.id = "story"
-const storyTell = document.createTextNode("Juicy, bone-in and extra marbled for maximum tenderness. Our chefs recommend ordering these cuts in the Wood-Fire Grill preparation: seasoned and flame grilled over oak wood for a delicious natural flavor.")
-story.appendChild(storyTell);
-const content = document.createTextNode("Juicy Steak");
-const buttonAddToCart = document.createElement('button');
-const buttonContainer = document.createElement('div');
-buttonContainer.id = "buttonContainer";
-buttonAddToCart.id = "buttonAddToCart";
-const buttonContent = document.createTextNode("ADD TO MY ORDER");
-buttonAddToCart.appendChild(buttonContent);
-buttonContainer.appendChild(buttonAddToCart);
+name.id = "talkTitle"
+const content = document.createTextNode("Talk App");
 name.appendChild(content);
 desc.appendChild(name);
-desc.appendChild(story);
-desc.appendChild(buttonContainer);
 const div = template.content.querySelector('div');
 div.appendChild(img);
 div.appendChild(desc);
